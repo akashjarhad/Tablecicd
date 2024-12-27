741fc1667899d8991d025a7820c27c8a607f4484
akashjarhad <akashjarhad7590@gmail.com>
2024-12-27 11:18:23 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -3,12 +3,12 @@ public static void main(String[] args) {
     for (int a = 1 ; a<=10 ; a++)
     {
         if (a % 2 ==0){
-        System.out.println(a);
+        
             continue;
-        }
-        else{
+        
         System.out.println(a);
-    }
+
+        }
     }
 }
 }
